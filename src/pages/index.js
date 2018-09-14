c67317c322bcc83abdfc3eb56ff9cb999def63e2
mikebutts <mgbutts@gmail.com>
2018-09-13 21:21:40 -0400

    added styling and images

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,15 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>new site with gatsby</h1>
-    <Link to="/page-2/">Go to page 2</Link>
+<div>
+    <div className="Hero">
+        <div className="HeroGroup">
+        <h1> some text here </h1>
+        <p> sodme other stuff </p>
+        <Link to="/video">Watch the video</Link>
+        </div>
+    </div>
+</div>
   </div>
 )
 
